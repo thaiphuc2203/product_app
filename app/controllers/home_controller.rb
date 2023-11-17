74a719634243de19270f5bf5bb6e926c252ac45c
thaiphuc2203 <thaiphuc2203@gmail.com>
2023-11-17 10:47:30 +0700

    add more config on home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user, only: [:index]
   def index
     @brands =  Brand.all
     render json: "wellcom to Thomas's App"
