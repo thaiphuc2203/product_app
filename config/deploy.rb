4cb6d0043569285f656b1058d4f773e7fb39b0f6
thaiphuc2203 <thaiphuc2203@gmail.com>
2023-11-23 17:01:54 +0700

    add

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,11 +52,16 @@ end
 
 namespace :deploy do
   desc 'Make sure local git is in sync with remote.'
-  task :check_revision do
+  task :force_bundle_install do
     on roles(:app) do
       within release_path do
         execute :rm, release_path.join('Gemfile.lock')
       end
+    end
+  end
+  
+  task :check_revision do
+    on roles(:app) do
       unless `git rev-parse HEAD` == `git rev-parse origin/deploy_original`
         puts 'WARNING: HEAD is not the same as origin/deploy_original'
         puts 'Run `git push` to sync changes.'
