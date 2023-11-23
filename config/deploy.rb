65b37785661a6b2919d9a9b2c8f97d5ecd82adcf
thaiphuc2203 <thaiphuc2203@gmail.com>
2023-11-23 13:43:21 +0700

    edit git branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,14 +21,14 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w[~/.ssh/id_rsa.pub] }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true # Change to false when not using ActiveRecord
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
+set :branch, 'deploy_original'
 # set :scm,           :git
-set :branch, :deploy_original
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
