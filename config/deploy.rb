e5ea05c0174c957b9abe167d5cf608f309670660
thaiphuc2203 <thaiphuc2203@gmail.com>
2023-11-23 16:01:13 +0700

    revert code

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,48 +9,22 @@ set :application,     'product_app'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :stage, :production
-set :rails_env, :production
-set :use_sudo, true
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :ssh_options,     { forward_agent: true, user: fetch(:user)}
+
 # Don't change these unless you know what you're doing
-# set :pty,             true
-# set :use_sudo,        true
-# set :stage,           :production
-# set :deploy_via,      :remote_cache
-# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_access_log, "#{release_path}/log/puma.error.log"
-# set :puma_error_log,  "#{release_path}/log/puma.access.log"
-# set :ssh_options,     { forward_agent: true, user: fetch(:user)}
-# set :puma_preload_app, true
-# set :puma_worker_timeout, nil
-# set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :pty, true
-set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
-set :assets_prefix, "prepackaged-assets"
-set :assets_manifests, ['app/assets/config/manifest.js']
-set :rails_assets_groups, :assets
-set :keep_releases, 5
-set :puma_rackup, -> { File.join(current_path, "config.ru") }
-set :puma_state, -> { "#{shared_path}/tmp/pids/puma.state" }
-set :puma_pid, -> { "#{shared_path}/tmp/pids/puma.pid" }
-set :puma_bind, -> { "unix://#{shared_path}/tmp/sockets/puma.sock" }
-set :puma_conf, -> { "#{shared_path}/puma.rb" }
-set :puma_access_log, -> { "#{shared_path}/log/puma_access.log" }
-set :puma_error_log, -> { "#{shared_path}/log/puma_error.log" }
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, "production"))
-set :rvm_map_bins, %w{gem rake ruby rails bundle}
-set :puma_threads, [0, 8]
-set :puma_workers, 0
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, false
-set :default_shell, '/bin/bash -l'
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
 set :branch, 'deploy_original'
