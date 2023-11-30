af2b4e4db915a1eb4fbf87471975a210f1e36710
thaiphuc2203 <thaiphuc2203@gmail.com>
2023-11-30 21:16:35 +0700

    add assets:precompile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user, only: [:index]
   def index
-    @brands =  Brand.all
-    render json: @brands
+    render json: 'welcome Thomas'
   end
 end
